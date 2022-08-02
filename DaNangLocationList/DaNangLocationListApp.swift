10f14ae062429e10120eab0f7c1cce489798609a
NguyenDangLamPhuong <67723724+NguyenDangLamPhuong@users.noreply.github.com>
2022-08-02 14:07:30 +0700

    Initial Commit

---
+++ DaNangLocationList/DaNangLocationListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaNangLocationListApp.swift
+//  DaNangLocationList
+//
+//  Created by Mac on 02/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct DaNangLocationListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
